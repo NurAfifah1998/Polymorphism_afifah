9e0738f421b5b4e25a53acc9f2cc03d76f781bd2
nurafifah98 <naafifah98@gmail.com>
2020-06-30 22:47:55 +0800

    membuat class lacimakeup, dengan atribut dan method

---
+++ src/LaciMakeup.java
@@ -0,0 +1,27 @@
+public class LaciMakeup {
+    //atribut
+    String nama_laci;
+    String nama_makeup;
+    String jenis_makeup;
+    int jumlah_makeup;
+//construktor
+    public LaciMakeup() {
+    }
+
+    public LaciMakeup(String nama_laci, String nama_makeup, String jenis_makeup, int jumlah_makeup) {
+        this.nama_laci = nama_laci;
+        this.nama_makeup = nama_makeup;
+        this.jenis_makeup = jenis_makeup;
+        this.jumlah_makeup = jumlah_makeup;
+    }
+
+    void tampilData_makeup(){
+        System.out.println("nama laci\t:"+nama_laci);
+        System.out.println("nama makeup\t:"+nama_makeup);
+        System.out.println("jenis makeup\t:"+jenis_makeup);
+        System.out.println("jumlah makeup\t"+jumlah_makeup);
+    }
+
+
+
+}

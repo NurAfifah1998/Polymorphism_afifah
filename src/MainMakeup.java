a2c269619982137bba01bdacdcce42adcf4cb5d0
nurafifah98 <naafifah98@gmail.com>
2020-06-30 22:48:35 +0800

    eksekusi MainMakeup

---
+++ src/MainMakeup.java
@@ -0,0 +1,17 @@
+public class MainMakeup {
+    public static void main(String[] args) {
+        LaciMakeup laciMakeup = new LaciMakeup();
+
+        laciMakeup.nama_laci="satu";
+        laciMakeup.nama_makeup="Lipstik Make Over";
+        laciMakeup.jenis_makeup="pelembab bibir";
+        laciMakeup.jumlah_makeup=3;
+        laciMakeup.tampilData_makeup();
+
+        //condtruktor
+
+
+
+
+    }
+}
